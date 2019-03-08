79e65bc19862c07f9847670a678321eaaf544490
Siddant <siddantgurung@hotmail.co.uk>
2019-03-08 08:22:08 +0000

    added to see the image in a layout

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+let parallax
+
+
 function scrollTop(e){
     e.preventDefault()
     window.scrollTo(0, 0)
 }
 
+function parallaxScroll(){
+    let scrolledHeight= window.pageYOffset
+    parallax.style.backgroundPositionY=scrolledHeight-2
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    parallax = document.querySelector('.parallax')
+    window.addEventListener('scroll', parallaxScroll)
+
+})
