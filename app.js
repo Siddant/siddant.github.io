d7e65301546cbb09bb7d8a5782abd7d8391d5735
Siddant <siddantgurung@hotmail.co.uk>
2019-03-12 19:28:41 +0000

    few changes to the styling

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-let parallax
+let parallax;
 
-
-function scrollTop(e){
-    e.preventDefault()
-    window.scrollTo(0, 0)
+function scrollTop(e) {
+	e.preventDefault();
+	window.scrollTo(0, 0);
 }
 
-function parallaxScroll(){
-    let scrolledHeight= window.pageYOffset
-    parallax.style.backgroundPositionY=scrolledHeight-2
+function parallaxScroll() {
+	let scrolledHeight = window.pageYOffset;
+	parallax.style.backgroundPositionY = scrolledHeight - 2;
 }
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    parallax = document.querySelector('.parallax')
-    window.addEventListener('scroll', parallaxScroll)
-
-})
+document.addEventListener('DOMContentLoaded', () => {
+	parallax = document.querySelector('.parallax');
+	window.addEventListener('scroll', parallaxScroll);
+});
