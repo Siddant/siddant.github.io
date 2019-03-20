33491fe3ed0399e9346c7dec52de1e96eb59e8a6
Siddant <siddantgurung@hotmail.co.uk>
2019-03-20 14:37:56 +0000

    navigation works on the mobile

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const navigationBar = document.querySelector('.navbar-start');
+    const navigationBarItems = document.querySelectorAll('.navbar-start .navbar-item');
+    console.log(navigationBarItems)
 
     function toggleHamburger() {
         if (hamburger.classList.length > 2) {
             hamburger.classList.remove('is-active')
             navigationBar.style.height = '0vh'
             navigationBar.style.fontSize = '0vh'
-
 
         } else {
             hamburger.classList.add('is-active')
