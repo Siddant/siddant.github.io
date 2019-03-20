f41a4bd63bf8b6a775c14dfcd2abbc468b36f645
Siddant <siddantgurung@hotmail.co.uk>
2019-03-20 11:19:26 +0000

    added a toggle on the mobile

--- app.js
+++ app.js
@@ -18,12 +18,16 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
+    const navigationBar = document.querySelector('.navbar-start');
 
     function toggleHamburger() {
         if (hamburger.classList.length > 2) {
             hamburger.classList.remove('is-active')
+            navigationBar.style.display = 'none'
+
         } else {
             hamburger.classList.add('is-active')
+            navigationBar.style.display = 'flex'
 
         }
     }
@@ -31,6 +35,5 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(hamburger)
 
     hamburger.addEventListener('click', toggleHamburger);
-
 
 })
