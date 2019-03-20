ad967b0fc4a0ac58735a1ef2305b2212a7702cf0
Siddant <siddantgurung@hotmail.co.uk>
2019-03-20 13:11:09 +0000

    changing the height

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// let parallax;
-
-// function scrollTop(e) {
-// 	e.preventDefault();
-// 	window.scrollTo(0, 0);
-// }
-
-// function parallaxScroll() {
-// 	let scrolledHeight = window.pageYOffset;
-// 	parallax.style.backgroundPositionY = scrolledHeight - 2;
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-// 	parallax = document.querySelector('.parallax');
-// 	window.addEventListener('scroll', parallaxScroll);
-// });
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const navigationBar = document.querySelector('.navbar-start');
@@ -29,8 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
             navigationBar.style.display = 'flex'
         }
     }
-
-    console.log(hamburger)
 
     hamburger.addEventListener('click', toggleHamburger);
 
