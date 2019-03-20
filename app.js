9b1c331334e2f1b65e0e30eaeb487037af80aa75
Siddant <siddantgurung@hotmail.co.uk>
2019-03-20 12:18:48 +0000

    tryin to use a slider animation on navigation

--- app.js
+++ app.js
@@ -24,11 +24,9 @@ document.addEventListener('DOMContentLoaded', () => {
         if (hamburger.classList.length > 2) {
             hamburger.classList.remove('is-active')
             navigationBar.style.display = 'none'
-
         } else {
             hamburger.classList.add('is-active')
             navigationBar.style.display = 'flex'
-
         }
     }
 
