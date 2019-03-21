dcbe2d2cf0e2557cb847ad42d5e958d2ad04b3b4
Siddant <siddantgurung@hotmail.co.uk>
2019-03-21 18:09:37 +0000

    added a scroll event handler

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
+    const navigation = document.querySelector('.navbar');
+
     const navigationBar = document.querySelector('.navbar-start');
     const navigationBarItems = document.querySelectorAll('.navbar-start .navbar-item');
+    const section = document.querySelectorAll('.section');
+    let nav_height = navigation.offsetHeight;
+
     console.log(navigationBarItems)
+    console.log(section)
+    console.log(nav_height)
+    console.log(this)
 
     function toggleHamburger() {
         if (hamburger.classList.length > 2) {
@@ -17,7 +25,35 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function handleScrollEvent() {
+        console.log('here')
+    }
+
     hamburger.addEventListener('click', toggleHamburger);
+    window.addEventListener('scroll', handleScrollEvent);
+
     AOS.init();
 
 })
+
+
+/*var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+}); */
