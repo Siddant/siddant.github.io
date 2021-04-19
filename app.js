61abae5b19f475ccee6b950868e220ba445d9a60
Siddant Gurung <siddant.grg@gmail.com>
2021-04-19 16:13:42 +0100

    removed the cname

--- app.js
+++ app.js
@@ -1,70 +1,72 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navigation = document.querySelector('.navbar');
+  const hamburger = document.querySelector('.hamburger')
+  const navigation = document.querySelector('.navbar')
 
-    const navigationBar = document.querySelector('.navbar-start');
-    const navigationBarItems = document.querySelectorAll('.navbar-item');
+  const navigationBar = document.querySelector('.navbar-start')
+  const navigationBarItems = document.querySelectorAll('.navbar-item')
 
-    const sections = document.querySelectorAll('.section');
-    const navHeight = navigation.offsetHeight;
-    const heroHeight = sections[0].offsetHeight / 8
+  const sections = document.querySelectorAll('.section')
+  const navHeight = navigation.offsetHeight
+  const heroHeight = sections[0].offsetHeight / 8
 
-    console.log(navigationBarItems[0].style.fontSize)
-    // navigationBarItems[0].style.fontSize = 1 + 'vh'
+  console.log(navigationBarItems[0].style.fontSize)
+  // navigationBarItems[0].style.fontSize = 1 + 'vh'
 
-    function toggleHamburger() {
+  function toggleHamburger() {
+    hamburger.classList.toggle('is-active')
+    navigationBar.classList.toggle('height-size')
 
+    // if (hamburger.classList.length > 2) {
+    //     hamburger.classList.remove('is-active')
+    //     navigationBar.classList.remove('height-size')
 
-        hamburger.classList.toggle('is-active');
-        navigationBar.classList.toggle('height-size');
+    //     // navigationBar.style.height = '0vh'
+    //     // navigationBar.style.display = 'none'
 
-        // if (hamburger.classList.length > 2) {
-        //     hamburger.classList.remove('is-active')
-        //     navigationBar.classList.remove('height-size')
+    // } else {
+    //     hamburger.classList.add('is-active')
+    //     // navigationBar.style.height = '42vh'
+    //     navigationBar.classList.add('height-size')
 
-        //     // navigationBar.style.height = '0vh'
-        //     // navigationBar.style.display = 'none'
+    //     // navigationBar.style.display = 'flex'
 
-        // } else {
-        //     hamburger.classList.add('is-active')
-        //     // navigationBar.style.height = '42vh'
-        //     navigationBar.classList.add('height-size')
+    // }
+  }
 
-        //     // navigationBar.style.display = 'flex'
-
-        // }
+  function handleScrollEvent() {
+    let currentPostion =
+      document.documentElement.scrollTop || document.body.scrollTop
+    let naviagtionfontSize = navigationBarItems[0].style.fontSize
+    if (currentPostion >= heroHeight) {
+      // navigationBarItems.forEach(elem => elem.style.fontSize = 2 + 'vh')
+      navigationBar.classList.add('scroll-activate')
+      navigation.classList.add('shadow')
+    } else {
+      navigationBar.classList.remove('scroll-activate')
+      navigation.classList.remove('shadow')
     }
 
+    sections.forEach((elem) => {
+      let top = elem.offsetTop - navHeight
+      let bottom = elem.offsetHeight + top
+      if (
+        (currentPostion >= top && currentPostion <= bottom) ||
+        (elem.className == 'footer section' &&
+          currentPostion + 200 >= top &&
+          currentPostion + 200 <= bottom)
+      ) {
+        navigation
+          .getElementsByClassName('active')[0]
+          .classList.remove('active')
+        document
+          .querySelector(`[href='#${elem.getAttribute('id')}']`)
+          .classList.add('active')
+      }
+    })
+  }
 
-    function handleScrollEvent() {
+  hamburger.addEventListener('click', toggleHamburger)
+  window.addEventListener('scroll', handleScrollEvent)
 
-        let currentPostion = document.documentElement.scrollTop || document.body.scrollTop
-        let naviagtionfontSize = navigationBarItems[0].style.fontSize
-        if (currentPostion >= heroHeight) {
-            // navigationBarItems.forEach(elem => elem.style.fontSize = 2 + 'vh')
-            navigationBar.classList.add('scroll-activate')
-            navigation.classList.add('shadow')
-
-
-        } else {
-            navigationBar.classList.remove('scroll-activate')
-            navigation.classList.remove('shadow')
-        }
-
-        sections.forEach(elem => {
-            let top = elem.offsetTop - navHeight
-            let bottom = elem.offsetHeight + top
-            if (currentPostion >= top && currentPostion <= bottom || (elem.className == 'footer section' && currentPostion + 200 >= top && currentPostion + 200 <= bottom)) {
-                navigation.getElementsByClassName('active')[0].classList.remove('active');
-                document.querySelector(`[href='#${elem.getAttribute('id')}']`).classList.add('active');
-            }
-        })
-    }
-
-    hamburger.addEventListener('click', toggleHamburger);
-    window.addEventListener('scroll', handleScrollEvent);
-
-    AOS.init({
-    });
-
+  AOS.init({})
 })
